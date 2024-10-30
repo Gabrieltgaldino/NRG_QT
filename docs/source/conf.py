42e29c6a369cc0f6f3e45285e18f6d7a00cf98c4
Gabrieltgaldino <56210753+Gabrieltgaldino@users.noreply.github.com>
2024-10-29 21:54:52 -0400

    teste

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "_static/images/main_menu/logo.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
