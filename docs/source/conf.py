bd29ea9bd631a230c3486efc58133749ede7b22e
Thomas DesCoteaux <thomas.descoteaux@umontreal.ca>
2025-04-18 03:22:01 -0400

    simplify tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ copyright = '2024, DesCoteaux, Galdino, Teruel and Najmanovich'
 author = 'DesCoteaux, Galdino, Teruel and Najmanovich'
 
 release = '0.1'
-version = '2025.14.1'
+version = '2025.14.2'
 
 # -- General configuration
 
